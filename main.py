fdf9ed3666681a37aa5b498ebf54f4738906194a
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-27 01:48:21 -0500

    created a function that compares resources and coffee requirements

--- main.py
+++ main.py
@@ -21,16 +21,37 @@ def show_resources():
                 print(f"\nWater: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g\n")
                 # print(f"Money: {cost}")    
 
-
+def check_enough_resources(order_ingredients):
+        for item in order_ingredients:
+            if order_ingredients[item] > resources[item]:
+                  print(f"Sorry, there is not enough {item}.")
+                  return False
+        return True
 
 
 # While machine_on is True, loop through the prompt
 while machine_on:
     choice = input(f"  What would you like? (espresso/latte/cappuccino): ").lower()
 
+    # If choice is off, exit while loop
     if choice == "off":
         machine_on = False
     
+    # If choice is report, display the amount of resources in the coffee machine
     elif choice == "report":
           show_resources()
     
+    elif choice == "latte" or choice == "espresso" or choice == "cappuccino":
+          drink = menu[choice]
+
+          if check_enough_resources(drink['ingredients']):
+                
+          
+    
+    else:
+          print("Invalid input, please try again.")
+
+    
+
+
+
