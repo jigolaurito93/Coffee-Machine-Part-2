a3cad3451d4dc37e0548ec6aa8175f1d4666b3eb
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-27 02:35:23 -0500

    created a function that subtract the resources from the required ingredients

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math
 # Flag to indicate whether the loop runs 
 machine_on = True
 
-money_inside = None
+money_inside = 0
 
 # Function to display the amount of resources in the coffee machine
 def show_resources():
@@ -13,20 +13,48 @@ def show_resources():
     water = resources["water"]
     milk = resources["milk"]
     coffee = resources["coffee"]
+    global money_inside
     
     # condition to show if report is called from the prompt
     if not money_inside:
         print(f"\nWater: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g\n")
     elif money_inside:
-                print(f"\nWater: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g\n")
-                # print(f"Money: {cost}")    
+                print(f"\nWater: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g")
+                print(f"Money: ${money_inside:.2f}")    
 
 def check_enough_resources(order_ingredients):
+# Function to compare the current state of the resources and the requirement for the chosen coffee
         for item in order_ingredients:
             if order_ingredients[item] > resources[item]:
                   print(f"Sorry, there is not enough {item}.")
                   return False
         return True
+
+def coins():
+    # Returns total amount of coins inserted
+    print(f"\nPlease insert coins.")
+    total = float(input(f"  how many quarters?: ")) * 0.25
+    total += float(input(f"  how many dimes?: ")) * 0.1
+    total += float(input(f"  how many nickels?: ")) * 0.05
+    total += float(input(f"  how many pennies?: ")) * 0.01
+    return total
+
+def is_transaction_successful(payment, drink_cost):
+    if payment < drink_cost:
+        print(f"Sorry that's not enough money. Money refunded.")
+        machine_online = False
+    else:
+         global money_inside
+         money_inside += payment
+         user_change = payment - drink_cost
+         money_inside -= user_change
+         print(f"\nHere is ${user_change:.2f} dollars in change.")
+
+def make_coffee(drink_name, order_ingredients):
+     for item in order_ingredients:
+          resources[item] -= order_ingredients[item]
+
+
 
 
 # While machine_on is True, loop through the prompt
@@ -45,6 +73,14 @@ while machine_on:
           drink = menu[choice]
 
           if check_enough_resources(drink['ingredients']):
+                payment = coins()
+                drink_cost = drink['cost']
+                is_transaction_successful(payment, drink_cost)
+                make_coffee(choice, drink['ingredients'])
+
+                
+
+
                 
           
     
