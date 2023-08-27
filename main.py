7132e7abc8b4da451ed7d3420736382520e4909f
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-27 01:17:55 -0500

    created a Function to display the amount of resources in the coffee machine

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from menu import menu
+from menu import menu, resources
 
 import math
 
 # Flag to indicate whether the loop runs 
 machine_on = True
+
+money_inside = None
+
+# Function to display the amount of resources in the coffee machine
+def show_resources():
+    # Variable to represent the current state of the resources
+    water = resources["water"]
+    milk = resources["milk"]
+    coffee = resources["coffee"]
+    
+    # condition to show if report is called from the prompt
+    if not money_inside:
+        print(f"\nWater: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g\n")
+    elif money_inside:
+                print(f"\nWater: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g\n")
+                # print(f"Money: {cost}")    
+
+
 
 
 # While machine_on is True, loop through the prompt
@@ -13,4 +31,6 @@ while machine_on:
     if choice == "off":
         machine_on = False
     
-    elif choice == "report" 
+    elif choice == "report":
+          show_resources()
+    
