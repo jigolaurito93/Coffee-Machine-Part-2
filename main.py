2a6bcfca1674cf7baa1cd3971ac3d01c948cf75e
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-27 01:07:36 -0500

    added the requirements

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from menu import menu
 
 import math
 
-machine_off = False
+# Flag to indicate whether the loop runs 
+machine_on = True
 
-while not machine_off:
+
+# While machine_on is True, loop through the prompt
+while machine_on:
     choice = input(f"  What would you like? (espresso/latte/cappuccino): ").lower()
+
+    if choice == "off":
+        machine_on = False
+    
+    elif choice == "report" 
